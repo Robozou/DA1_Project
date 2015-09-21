f3204298c3307b3343347ba6e22500905025ac63
Robozou <201404905@post.au.dk>
2015-09-21 13:18:05 +0200

    Login content Menu + content Maps + key

--- app/src/main/java/g5/da1/da1_project/LoginActivity.java
+++ app/src/main/java/g5/da1/da1_project/LoginActivity.java
@@ -1,25 +1,23 @@
 package g5.da1.da1_project;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-//import com.firebase.client.Firebase;
+import com.firebase.client.Firebase;
 
 public class LoginActivity extends AppCompatActivity {
     Button loginBut, registerBut;
     EditText usernameEdit, passwordEdit;
-    //Firebase myFirebaseRef;
+    Firebase myFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Firebase.setAndroidContext(this);
-        //myFirebaseRef = new Firebase("https://shining-inferno-7431.firebaseio.com/");
+        Firebase.setAndroidContext(this);
+        myFirebaseRef = new Firebase("https://shining-inferno-7431.firebaseio.com/");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         loginBut = (Button) findViewById(R.id.loginButton);
@@ -27,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
             Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
             }
         });
         registerBut = (Button) findViewById(R.id.registerButton);
